5c582523a9a83ff86954dddf0408c5c42ae24887
Aliendreamer <dreamingman83@gmail.com>
2019-07-06 14:59:21 +0300

    lecture 180 copied all his comments on code too

--- maxPythonAndBlockchain/oop/block.py
+++
@@ -1,9 +0,0 @@
-from time import time
-
-class Block:
-      def __init__(self,index,previous_hahs,transactions,proof,time=time()):
-          self.index=index
-          self.previous_hash=previous_hahs
-          self.transactions=transactions
-          self.proof=proof
-          self.time=time
