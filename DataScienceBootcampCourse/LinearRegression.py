acf95c8051e7e68cf20c2fef5f1471258ed3ef0b
Aliendreamer <dreamingman83@gmail.com>
2019-08-03 20:27:21 +0300

    fun starts here :D

---
+++ DataScienceBootcampCourse/LinearRegression.py
@@ -0,0 +1,41 @@
+#%% 
+import seaborn as sns
+import scipy
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+#%%
+data=pd.read_csv(r'DataScienceBootcampCourse\csvFiles\real_estate_price_size.csv')
+print(data)
+
+#%%
+data.describe()
+
+
+#%%
+y=data['size']
+x1=data['price']
+plt.scatter(x1,y)
+plt.xlabel("price",fontsize=20)
+plt.ylabel('size',fontsize=20)
+plt.show()
+
+#%%
+x=sm.add_constant(x1)
+#%%
+results=sm.OLS(y,x).fit()
+
+results.summary()
+
+#%%
+plt.scatter(x1,y)
+yhat=0.0033*x1+(-122.3349)
+fig=plt.plot(x1,yhat,lw=4,c='red',label='line')
+plt.xlabel("price",fontsize=20)
+plt.ylabel('size',fontsize=20)
+plt.show()
+
+
+#%%
