5f3fa92f4d93dc48a9defb66c5f193c01afc6e27
Aliendreamer <dreamingman83@gmail.com>
2019-08-04 12:49:59 +0300

    up to lesson 207 section 34

--- DataScienceBootcampCourse/LinearRegression.py
+++ DataScienceBootcampCourse/LinearRegression.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
-
+sns.set()
 #%%
 data=pd.read_csv(r'DataScienceBootcampCourse\csvFiles\real_estate_price_size.csv')
 print(data)
